a6ccd918920e6437ad9d9d251020a98a5a7fe454
Sumit Ridhal <sumitridhal@gmail.com>
2017-07-18 18:31:06 +0530

    Luhn Algorithm update

--- app.js
+++ app.js
@@ -74,8 +74,39 @@ Number.prototype.between = function(a, b) {
   return this >= min && this <= max;
 };
 
+function Luhn(card) {
+  var i, n, c, r, t;
+  r = card.split('-').join('');
+  // Run through each single digit to create a new string. Even digits
+  // are multiplied by two, odd digits are left alone.
+
+  t = "";
+  for (i = 0; i < r.length; i++) {
+    c = parseInt(r.charAt(i), 10);
+    if (i % 2 != 0)
+      c *= 2;
+    t = t + c;
+  }
+
+  // Finally, add up all the single digits in this string.
+  n = 0;
+  for (i = 0; i < t.length; i++) {
+    c = parseInt(t.charAt(i), 10);
+    n = n + c;
+  }
+
+
+  if (n != 0 && n % 10 == 0)
+    return true;
+  else
+    return false;
+}
+
+
+var card_issuer = '';
+
 function validator(card) {
-  let result = 'Invalid Card'
+  let result = false
   let num = card.split('-').join('');
   let len = num.length;
   for (let key in card_meta) {
@@ -90,7 +121,8 @@ function validator(card) {
           let objl = [...obj['length']];
           if (typeof prefix == 'number') {
             if (num.startsWith(prefix) == true && objl.includes(len)) {
-              result = obj['Credit Card Issuer']
+              card_issuer = obj['Credit Card Issuer']
+              result = true;
               abort2 = true;
               abort3 = true;
               break;
@@ -104,7 +136,8 @@ function validator(card) {
             let snum = parseInt(num.substring(0, sdigit));
             // let pnum = parseInt(num)
             if (snum.between(range[0], range[1]) == true && objl.includes(len)) {
-              result = obj['Credit Card Issuer']
+              card_issuer = obj['Credit Card Issuer']
+              result = true;
               abort2 = true;
               abort3 = true;
               break;
@@ -123,12 +156,22 @@ function validator(card) {
       break;
     }
   }
-
   return result;
 }
 
 rl.question('Enter Credit Card Number(####-####-####-####): ', (n) => {
   let res = validator(n)
-  console.log(`${res}`)
+  if (res) {
+    let rev = n.split("").reverse().join("")
+    res = Luhn(rev)
+    if (res) {
+      console.log(`${card_issuer}`)
+    } else {
+      console.log('Invalid Card')
+    }
+  } else {
+    console.log('Invalid Card')
+  }
+
   rl.close()
 })
